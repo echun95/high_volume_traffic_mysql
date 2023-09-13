ab33ecceeae0739b16124a2ccc73cfd9a58f2955
leeyoungchun <dldydcns123@gmail.com>
2023-09-13 23:02:46 +0900

    [feat] member 등록, 조회 api 구현
    1. member 등록, 조회 api 구현 완료

--- src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -2,16 +2,40 @@ package com.example.fastcampusmysql.domain.member.repository;
 
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.plaf.PanelUI;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final static String TABLE = "Member";
+    public Optional<Member> findById(Long id){
+        String sql = String.format("SELECT * FROM %s WHERE id = :id", TABLE);
+        MapSqlParameterSource param = new MapSqlParameterSource().addValue("id",id);
+        RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum) -> Member
+                .builder()
+                .id(resultSet.getLong("id"))
+                .email(resultSet.getString("email"))
+                .nickname(resultSet.getString("nickname"))
+                .birthday(resultSet.getObject("birthday", LocalDate.class))
+                .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+                .build();
+        Member findMember = namedParameterJdbcTemplate.queryForObject(sql, param, rowMapper);
+        return Optional.ofNullable(findMember);
+    }
+
     public Member save(Member member){
         /*
             member id를 보고 갱신 또는 삽입을 정함
@@ -25,7 +49,7 @@ public class MemberRepository {
 
     private Member insert(Member member){
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
-                .withTableName("Member")
+                .withTableName(TABLE)
                 .usingGeneratedKeyColumns("id");
         SqlParameterSource params = new BeanPropertySqlParameterSource(member);
         Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
